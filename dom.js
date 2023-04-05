7e43ee7b87ff67cf9c9c871aad32f722d2573b1f
Suryansh-chauhan <chauhanaman1912@gmail.com>
2023-04-06 00:53:04 +0530

    2nd commit

---
+++ dom.js
@@ -0,0 +1,19 @@
+//examining document object
+// console.dir(document);
+
+// console.log("hello");
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+// document.title=123;
+
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+
+// getelement by id 
+console.log(document.getElementById('header-title'));
+var id_main = document.getElementById('main');
+
