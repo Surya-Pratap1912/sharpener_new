6803fc1ad42e97232a0116af6c70b78e1b04c1ca
Surya Pratap Singh <81734074+Suryansh-chauhan@users.noreply.github.com>
2023-07-30 19:48:56 +0530

    yes

--- signup/server/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize =  new Sequelize('node_test_1', 'root', 'root', {
-    dialect : 'mysql',
-    host : 'localhost'
-});
-
-module.exports = sequelize;
